766e4cadbcc18b0a6e7e92ec2417daf238ca5ae0
startupsolellc <125030964+startupsolellc@users.noreply.github.com>
2025-02-17 21:22:36 +0300

    Create env.js

---
+++ functions/env.js
@@ -0,0 +1,8 @@
+window.env = {
+    FIREBASE_API_KEY: "AIzaSyCX01FDEWQFL5zE6BZy45GHFuMFo0Z0cLg",
+    FIREBASE_AUTH_DOMAIN: "brandgenixapp.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "brandgenixapp",
+    FIREBASE_STORAGE_BUCKET: "brandgenixapp.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID: "1065728742460",
+    FIREBASE_APP_ID: "1065728742460"
+};
